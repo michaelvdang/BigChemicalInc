dcea031af1e39719b6b3752c193721fe80f44add
Michael Dang <mdang2023@gmail.com>
2022-05-07 01:59:52 -0700

    added CORS middleware

--- main.py
+++ main.py
@@ -2,6 +2,24 @@ from typing import Optional
 import sqlite3
 from fastapi import FastAPI, Depends
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
+import contextlib
+
+app = FastAPI()
+
+origins = [
+    "http://localhost:4200",
+    # "http://localhost:*",
+    # "http://127.0.0.1:4200"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    # allow_credentials=True,
+    allow_methods=["PATCH"], # does this work ?
+    # allow_headers=["*"],
+)
 
 class TestResult(BaseModel):
   employeeID: int
@@ -12,10 +30,8 @@ class TestResult(BaseModel):
   results: bool
   comments: str
 
-app = FastAPI()
-
-def get_db():
-  with sqlite3.connect('var/BigChemicalInc.db') as db:
+async def get_db():
+  with contextlib.closing(sqlite3.connect('var/BigChemicalInc.db', check_same_thread=False)) as db:
     yield db
 
 @app.get("/")
